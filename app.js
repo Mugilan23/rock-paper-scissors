284d291e77677f0dfd129abbd106a5daf8d1cc92
Mugilan <mugilanmass23@gmail.com>
2019-06-29 12:25:00 +0530

    Add playRound function

--- app.js
+++ app.js
@@ -5,10 +5,37 @@ function computerPlay() {
 	return computerChoice;
 }
 
-function playRound(playerSelection, computerSelection) {
-	return [ playerSelection, computerSelection ];
+function capitalizeFirstLetter(string) {
+	return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
-const playerSelection = 'ROCK';
+function playRound(playerSelection, computerSelection) {
+	let player = capitalizeFirstLetter(playerSelection.toLowerCase());
+	let computer = capitalizeFirstLetter(computerSelection.toLowerCase());
+	let result = getResult(player, computer);
+	return result;
+}
+
+function getResult(playerChoice, computerChoice) {
+	if (playerChoice === computerChoice) {
+		return 'Draw';
+	} else if (
+		(playerChoice === 'Scissors' && computerChoice === 'Paper') ||
+		(playerChoice === 'Paper' && computerChoice === 'Rock') ||
+		(playerChoice === 'Rock' && computerChoice === 'Scissors')
+	) {
+		return `You Win!, ${playerChoice} beats ${computerChoice}`;
+	} else if (
+		(computerChoice === 'Scissors' && playerChoice === 'Paper') ||
+		(computerChoice === 'Paper' && playerChoice === 'Rock') ||
+		(computerChoice === 'Rock' && playerChoice === 'Scissors')
+	) {
+		return `You Lose!, ${computerChoice} beats ${playerChoice}`;
+	} else {
+		return `${playerChoice} is invalid`;
+	}
+}
+
+const playerSelection = 'paper';
 const computerSelection = computerPlay();
 console.log(playRound(playerSelection, computerSelection));
