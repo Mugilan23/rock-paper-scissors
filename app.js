1b92aaeca7b7f3ff1f78e1c4066c35c4227244b3
Mugilan <mugilanmass23@gmail.com>
2019-07-11 19:53:24 +0530

    completed index.html and style.css

--- app.js
+++ app.js
@@ -1,29 +1,5 @@
-const options = [ 'rock', 'paper', 'scissors' ];
+let userScore = 0;
+let computerScore = 0;
 
-let pDisplay = document.getElementById('player-choice');
-let cpuDisplay = document.getElementById('computer-choice');
-
-// creates new buttons based off options
-let optionsDiv = document.getElementById('playing-choices');
-options.forEach((option) => {
-	let newBtn = document.createElement('button');
-	newBtn.innerText = option;
-	optionsDiv.append(newBtn);
-});
-
-let buttons = document.querySelectorAll('#playing-choices button');
-buttons.forEach((btn) =>
-	btn.addEventListener('click', (e) => {
-		playRound(e.target.textContent);
-	})
-);
-
-function computerPlay() {
-	let computerChoice = options[Math.floor(Math.random() * options.length)];
-	return computerChoice;
-}
-
-function playRound(playerSelection) {
-	pDisplay.textContent = playerSelection;
-	cpuDisplay.innerText = computerPlay();
-}
+const userScore_span = document.getElementById('user-score');
+const computerScore_span = document.getElementById('computer-score');
